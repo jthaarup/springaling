7b638321e62909c91ca96519b7a3e67eb5388212
Jeppe Thaarup <jeth@jysk.com>
2023-06-19 10:52:37 +0200

    initial

---
+++ src/main/java/com/example/springaling/SpringalingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springaling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringalingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringalingApplication.class, args);
+	}
+
+}
